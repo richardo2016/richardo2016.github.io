fa7ff5d4efc151337e19964721613044c9e94614
richardo2016 <richardo2016@gmail.com>
2021-05-20 17:48:36 +0800

    Updates

--- page-blobs/abuse-of-destructing-assignment/sample.js
+++
@@ -1,5 +0,0 @@
-// 解构数组
-const [a, b, ...restItems] = arr || []
-
-// 解构对象
-const {a, b, ...restProps} = obj || {}
--- page-blobs/left-displacement/experimenet-one-step.js
+++
@@ -1,3 +0,0 @@
-function leftDisplacement (list, n = 0) {
-  return (n = n % list.length, !n ? list : list.slice(-n).concat(list.slice(0, -n)))
-}
